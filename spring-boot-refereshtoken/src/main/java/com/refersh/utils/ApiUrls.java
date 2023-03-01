d48904f92c6c6bdd07fd92cfbf328008378a1f90
deepakbehera1730 <118535168+deepakbehera1730@users.noreply.github.com>
2023-03-01 17:36:51 +0530

    Add files via upload

---
+++ spring-boot-refereshtoken/src/main/java/com/refersh/utils/ApiUrls.java
@@ -0,0 +1,10 @@
+package com.refersh.utils;
+
+public class ApiUrls {
+
+	public static final String AUTH = "/auth";
+	public static final String LOGIN = AUTH + "/login";
+	public static final String REFRESH_TOKEN = AUTH + "/refresh-token";
+	public static final String REGISTRATION = "/registration";
+	public static final String USER = "/users";
+}
